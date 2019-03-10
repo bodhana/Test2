6821581626fbcaa64232cc8931859bc9fb853432
bodhana <vvbodhana@gmail.com>
2019-03-10 11:40:01 +0530

    Sum of abc

---
+++ abc.java
@@ -0,0 +1,12 @@
+class abc
+{
+	public static void  main(String[] args)
+	{
+		int a,b,c;
+		a=500;
+		b=3000;
+		c=1200;
+		c=a+b+c;
+		System.out.println("The sum of three values are "+c);
+	}
+	}
